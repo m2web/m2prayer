f6d99661859a0547efda20368cb88656cae5c0ef
m2web <m2web@yahoo.com>
2014-12-05 06:48:46 -0500

    Reverted to using a server method call to get the needed value from the REST webservice

--- client/templates/todaysVerse.js
+++ client/templates/todaysVerse.js
@@ -1,7 +1,17 @@
 Template.todaysVerse.helpers({
   theVerse: function() {
-	var esv = new ESV();
-	return esv.getTodaysVerse();
+	//put something in the returned value to show
+	var todaysVerseOutput = "There was a problem getting the verse."
+	//call to the todaysMemoryVerse method in the server/verses.js file
+	Meteor.call('todaysVerseMethod',function(err, response) {
+		//set the value in session
+		Session.set('todaysMemoryVerse', response);
+	});
+	//get the value out of session that was set in session above
+	todaysVerseOutput = Session.get('todaysMemoryVerse')
+	//content is the key of the verse text in the JSON document
+	return todaysVerseOutput['content'];
   }
 });
+
 
--- server/verses.js
+++ server/verses.js
@@ -2,3 +2,12 @@ Meteor.publish('memoryVerses', function() {
 	console.log(this.jm_verses);
 	return jm_verses.find({});
 });
+
+Meteor.startup(function () {
+	Meteor.methods({
+	  todaysVerseMethod: function (name) {
+		var esv = new ESV();
+		return esv.getTodaysVerse();
+	  }
+	});
+  });
