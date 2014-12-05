6f2854b39f6fa604e718eae9ac50761fb265eec8
m2web <m2web@yahoo.com>
2014-12-05 17:53:18 -0500

    All sorts of stuff with both the server and client

--- client/templates/memoryVerses.js
+++ client/templates/memoryVerses.js
@@ -1,13 +1,19 @@
 Meteor.subscribe('memoryVerses');
 
 Template.memoryVerses.helpers({
-  memoryVerses: function() {
-	var todaysDate = new Date();
-	var lastMonth = todaysDate.getMonth();
-	var theYear = todaysDate.getFullYear();
-	//this is from the jm_verses (Joshua's Men) collection
-	//return memoryVerses.find({'year': theYear, 'month': {$gte: 0, $lte: lastMonth}});
-	//console.log(memoryVerses);
-	return MemoryVerses.find({});
-  }
+	memoryVerses: function() {
+		var todaysDate = new Date();
+		//The lastMonth is used to determine the number of memory verses to be displayed in the query below. If April 
+		//is the month, then verses up to and including April will be shown
+		//Also, since the last month for meeting to memorize scripture for Joshua's Men is November and the getMonth 
+		//returns a 0 - 11 number, we do not need to include December (or 12) and therefore do not need to add 1.
+		var lastMonth = todaysDate.getMonth() ;
+		//theYear is used to determine if the Joshua's Men group is using Grudem or the books. 
+		var theYear = todaysDate.getFullYear();
+		//even year (ex. 2014 and 2016) is using Grudem's. This is used in the query below
+		var bookYear = (theYear % 2 == 0) ? "GRUDEM":"BOOKS";
+		
+		//we are using the year ('GRUDEM' or 'BOOKS') and the month number range. We are sorting my month number and reference (verse location) descending.
+		return MemoryVerses.find({'year': bookYear, 'month': {$gte: 0, $lte: lastMonth}}, {sort: {'month': -1, 'reference': -1}});
+	}
 });
---
+++ client/templates/pageLinks.js
@@ -0,0 +1,32 @@
+Template.pageLinks.helpers({
+	thisMonthsReadingURL : function(){
+		var linkBase = "http://markmcfadden.net/prayerweb/DailyReading/";
+		var pageExtension = ".html";
+		var todaysDate = new Date();
+		//since the getMonth method returns a 0-11 number add 1 to get the current month number
+		var thisMonth = todaysDate.getMonth() + 1 ;
+		//the page is named for the month's number
+		return linkBase + thisMonth.toString() + pageExtension;
+	},
+	todaysValleyOfVisionPrayerURL : function(){
+		var linkBase = "http://markmcfadden.net/prayerweb/vov/";
+		var pageExtension = ".html";
+		var todaysDate = new Date();
+		//this date method is a custom method created in the lib/date_helper.js file. This is the day number fo the year (1 - 365)
+		var todaysVOVPrayerNumber = todaysDate.getDOY();
+		//the number of VOV prayers
+		var numberOfPrayers = 159;
+		//since there is more than 159 days in the year multiply it by 2
+		var numberOfPrayersX2 = numberOfPrayers * 2;
+		
+		//if today's number is more than the number of prayers but less than twice the number of prayers use the difference between teh two
+		if(todaysVOVPrayerNumber > numberOfPrayers && todaysVOVPrayerNumber <= numberOfPrayersX2) {
+			todaysVOVPrayerNumber = todaysVOVPrayerNumber - numberOfPrayers;
+		//if today's number is greater than twice the number of prayers get a random number of a prayer
+		}else if(todaysVOVPrayerNumber > numberOfPrayersX2) {
+			todaysVOVPrayerNumber = Math.floor(Math.random() * (numberOfPrayers - 1) )+ 1;
+		}
+		//the page is named for the prayer's number
+		return linkBase +  todaysVOVPrayerNumber + pageExtension;
+	}
+});
